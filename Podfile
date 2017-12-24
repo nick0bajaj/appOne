9aa5d4de8eb0977c89347adab1c59c76506e964f
nick0bajaj <nick.bajaj@berkeley.edu>
2017-12-24 02:26:01 -0500

    adding map search

--- Podfile
+++ Podfile
@@ -10,4 +10,5 @@ target 'Split' do
     pod 'Firebase/Storage'
     pod 'Firebase/Auth'
     pod 'Firebase/Database'
+    pod 'GooglePlaces'
 end
---
+++ Pods/Pods/Target Support Files/Pods-GoogleToolboxForMac/Pods-GoogleToolboxForMac-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GoogleToolboxForMac : NSObject
+@end
+@implementation PodsDummy_Pods_GoogleToolboxForMac
+@end
